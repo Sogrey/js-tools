a992ae94678931802c0bec24a2c21f53eba867f4
sogrey <suzhaoheng2006@163.com>
2018-11-24 09:45:51 +0800

    add js_rgb.js and paopao.js

--- js/js-cookieUtil.js
+++ js/js-cookieUtil.js
@@ -1,7 +1,8 @@
 /**
-     * [cookieUtil 操作cookie
-     * @type {Object}
-     */
+ * [cookieUtil 操作cookie
+ * @type {Object}
+ */
+; (function (win) {
     var cookieUtil = {
         /**
          * 增加cookie
@@ -9,39 +10,35 @@
          * @param {string} value      值
          * @param {number} expireTime 过期时间，ms
          */
-        setCookie:function(name, value, expireTime, path){
-            var cookiePath = path ? (";path=" + path) : "";
+        setCookie: function (name, value, expireTime) {
             var exp = new Date();
             exp.setTime(exp.getTime() + expireTime);
-            document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString() + cookiePath;
+            document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
         },
         /**
          * 获取cookie
          * @param  {string} name cookie名称
          * @return {string}      值
          */
-        getCookie:function(name){
-            var arr;
-            var reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
-            if(arr = document.cookie.match(reg)) {
+        getCookie: function (name) {
+            var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+            if (arr = document.cookie.match(reg))
                 return unescape(arr[2]);
-            }else{
+            else
                 return null;
-            }
-            return null;
         },
-
         /**
          * 删除cookie
          * @param  {string} name cookie名称
          */
-        delCookie:function(name, path){
+        delCookie: function (name) {
             var exp = new Date();
-            var cookiePath = path ? (";path=" + path) : "";
             exp.setTime(exp.getTime() - 1000000);
-            var cval=cookieUtil.getCookie(name);
-            if(cval !== null) {
-                document.cookie= name + "="+cval+";expires="+exp.toGMTString() + cookiePath;
+            var cval = cookieUtil.getCookie(name);
+            if (cval !== null) {
+                document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString() ;
             }
         }
     };
+    win.cookieUtil = cookieUtil;
+})(window);
